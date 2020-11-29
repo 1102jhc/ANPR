a102c3472f6922c420e995094faaed89e64095ce
1102jhc <73589005+1102jhc@users.noreply.github.com>
2020-11-30 01:13:36 +0900

    init

---
+++ Recognize_VehicleLicensePlate/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Recognize_VehicleLicensePlate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRecognize = new System.Windows.Forms.Button();
+            this.btnSector = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(274, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 265);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnRecognize
+            // 
+            this.btnRecognize.Location = new System.Drawing.Point(274, 349);
+            this.btnRecognize.Name = "btnRecognize";
+            this.btnRecognize.Size = new System.Drawing.Size(75, 23);
+            this.btnRecognize.TabIndex = 2;
+            this.btnRecognize.Text = "인식";
+            this.btnRecognize.UseVisualStyleBackColor = true;
+            this.btnRecognize.Click += new System.EventHandler(this.btnRecognize_Click);
+            // 
+            // btnSector
+            // 
+            this.btnSector.Location = new System.Drawing.Point(592, 349);
+            this.btnSector.Name = "btnSector";
+            this.btnSector.Size = new System.Drawing.Size(75, 23);
+            this.btnSector.TabIndex = 4;
+            this.btnSector.Text = "번호판인식";
+            this.btnSector.UseVisualStyleBackColor = true;
+            this.btnSector.Click += new System.EventHandler(this.btnSector_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(274, 389);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(393, 25);
+            this.textBox1.TabIndex = 5;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(558, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(513, 266);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1535, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSector);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRecognize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRecognize;
+        private System.Windows.Forms.Button btnSector;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Recognize_VehicleLicensePlate/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+using System.Net.Http;
+using System.IO;
+/*using System.Windows.Media.Imaging;*/
+
+namespace Recognize_VehicleLicensePlate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRecognize_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            var ocr = new TesseractEngine("./tessdata", "kor", EngineMode.TesseractAndLstm);
+            var texts = ocr.Process(img);
+            MessageBox.Show(texts.GetText());
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<string> ProcessImage(string image_path)
+        {
+            string SECRET_KEY = "키설정을 해주세요.";
+
+            Byte[] bytes = File.ReadAllBytes(image_path);
+            string imagebase64 = Convert.ToBase64String(bytes);
+
+            var content = new StringContent(imagebase64);
+
+            var response = await client.PostAsync("https://api.openalpr.com/v3/recognize_bytes?recognize_vehicle=1&country=kr&secret_key=" + SECRET_KEY, content).ConfigureAwait(false);
+
+            var buffer = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+            var byteArray = buffer.ToArray();
+            var responseString = Encoding.UTF8.GetString(byteArray, 0, byteArray.Length);
+
+            return responseString;
+        }
+
+        private void btnSector_Click(object sender, EventArgs e)
+        {
+            Task<string> recognizeTask = Task.Run(() => ProcessImage(@"C:\qqq.jpg"));
+            recognizeTask.Wait();
+            string task_result = recognizeTask.Result;
+
+            string result = task_result.Split(':')[11].Split(',')[0];
+
+            result = result.Replace("\"", "");
+
+            result = result.Trim();  // 결과 값을 적절히 파싱
+
+            textBox1.AppendText(result);
+
+           /* System.Console.WriteLine(task_result);
+            textBox1.Text = task_result;
+           */
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
